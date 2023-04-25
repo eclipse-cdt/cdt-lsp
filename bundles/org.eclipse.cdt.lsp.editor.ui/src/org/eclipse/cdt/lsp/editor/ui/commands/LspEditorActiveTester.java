2d677821e914a1fd1bfac23d13f9fc2155c92c4e
Dominic Scharfe <dominic.scharfe@coseda-tech.com>
2023-04-25 16:10:40 +0200

    [#47] Add action to switch between Source/Header
    - add server interface for clangd textDocument/switchSourceHeader
    extension
    - add command and menu entry for switch action
    - add orbit repository to target platform (required for Mockito)
    
    Note: In the UI the term "toggle" is used instead of switch
    in order to be consistent with the CDT editor.

---
+++ bundles/org.eclipse.cdt.lsp.editor.ui/src/org/eclipse/cdt/lsp/editor/ui/commands/LspEditorActiveTester.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2023 COSEDA Technologies GmbH and others.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * 
+ * Contributors:
+ * Dominic Scharfe (COSEDA Technologies GmbH) - initial implementation
+ *******************************************************************************/
+package org.eclipse.cdt.lsp.editor.ui.commands;
+
+import java.util.Optional;
+
+import org.eclipse.cdt.lsp.LspPlugin;
+import org.eclipse.core.expressions.PropertyTester;
+import org.eclipse.core.runtime.Adapters;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.texteditor.ITextEditor;
+
+/**
+ * Property tester for checking if a receiver is a c editor. Will evaluate
+ * {@code true} if the receiver can adapt to {@link ITextEditor} and has the c
+ * editor id.
+ */
+public class LspEditorActiveTester extends PropertyTester {
+	public final static String IS_LSP_CEDITOR_PROPERTY = "isLspCEditor";
+
+	@Override
+	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
+		if (receiver instanceof IEditorPart && IS_LSP_CEDITOR_PROPERTY.equals(property)) {
+			IEditorPart innerEditor = Optional.ofNullable((IEditorPart) Adapters.adapt(receiver, ITextEditor.class))
+					.orElse((IEditorPart) receiver);
+
+			return LspPlugin.LSP_C_EDITOR_ID.equals(innerEditor.getSite().getId());
+		}
+		return false;
+	}
+}
