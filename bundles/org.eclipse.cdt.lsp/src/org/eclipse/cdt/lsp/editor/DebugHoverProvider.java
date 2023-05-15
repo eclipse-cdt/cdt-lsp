aa2d76be0af73c1955188010f3e215efac86e992
Dominic Scharfe <dominic.scharfe@coseda-tech.com>
2023-05-15 16:51:35 +0200

    [#70] Add hover provider for debugging
    Adds a hover provider which delegates to the cdt debug hover mechanism.

---
+++ bundles/org.eclipse.cdt.lsp/src/org/eclipse/cdt/lsp/editor/DebugHoverProvider.java
@@ -0,0 +1,48 @@
+package org.eclipse.cdt.lsp.editor;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import org.eclipse.cdt.ui.text.c.hover.ICEditorTextHover;
+import org.eclipse.debug.ui.DebugUITools;
+import org.eclipse.jface.text.IInformationControlCreator;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextHover;
+import org.eclipse.jface.text.ITextHoverExtension;
+import org.eclipse.jface.text.ITextHoverExtension2;
+import org.eclipse.jface.text.ITextViewer;
+
+/**
+ * Delegates hover to the text hover implementation of cdt.
+ */
+public class DebugHoverProvider implements ITextHover, ITextHoverExtension, ITextHoverExtension2 {
+	@Override
+	public IInformationControlCreator getHoverControlCreator() {
+		return getDelegate(ITextHoverExtension.class).map(ITextHoverExtension::getHoverControlCreator).orElse(null);
+	}
+
+	@SuppressWarnings("deprecation")
+	@Override
+	public String getHoverInfo(ITextViewer textViewer, IRegion hoverRegion) {
+		return getDelegate(ITextHover.class).map(delegate -> delegate.getHoverInfo(textViewer, hoverRegion))
+				.orElse(null);
+	}
+
+	@Override
+	public IRegion getHoverRegion(ITextViewer textViewer, int offset) {
+		return getDelegate(ITextHover.class).map(delegate -> delegate.getHoverRegion(textViewer, offset)).orElse(null);
+	}
+
+	@Override
+	public Object getHoverInfo2(ITextViewer textViewer, IRegion hoverRegion) {
+		return getDelegate(ITextHoverExtension2.class).map(delegate -> delegate.getHoverInfo2(textViewer, hoverRegion))
+				.orElse(null);
+	}
+
+	private <T> Optional<T> getDelegate(Class<T> clazz) {
+		return Optional.ofNullable(DebugUITools.getDebugContext())
+				.map(adaptable -> adaptable.getAdapter(ICEditorTextHover.class)).filter(Objects::nonNull)
+				.filter(clazz::isInstance).map(clazz::cast);
+	}
+
+}
