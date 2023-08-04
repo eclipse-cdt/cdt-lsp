458362c84b5892fc7e6dfbdda158566dd661351a
Gesa Hentschke <123444711+ghentschke@users.noreply.github.com>
2023-08-04 11:32:41 +0200

    [#149] Add interface and service to change configuration visibility (#160)
    [#149] Add interface and service to change configuration visibility
    
    The new interface ClangdConfigurationVisibility allows vendors to hide
    the 'Prefer C/C++ Editor (LSP)' check-box and the 'clangd options' group
    in the preference/properties page.
    
    fixes #149 #153

---
+++ bundles/org.eclipse.cdt.lsp.clangd/src/org/eclipse/cdt/lsp/internal/clangd/editor/DefaultClangdConfigurationVisibility.java
@@ -0,0 +1,31 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Bachmann electronic GmbH and others.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ * Gesa Hentschke (Bachmann electronic GmbH) - initial implementation
+ *******************************************************************************/
+package org.eclipse.cdt.lsp.internal.clangd.editor;
+
+import org.eclipse.cdt.lsp.clangd.ClangdConfigurationVisibility;
+import org.osgi.service.component.annotations.Component;
+
+@Component(property = { "service.ranking:Integer=0" })
+public final class DefaultClangdConfigurationVisibility implements ClangdConfigurationVisibility {
+
+	@Override
+	public boolean showPreferClangd(boolean isProjectScope) {
+		return true;
+	}
+
+	@Override
+	public boolean showClangdOptions(boolean isProjectScope) {
+		return !isProjectScope; // TODO: return true when multiple LS per workspace are supported
+	}
+
+}
