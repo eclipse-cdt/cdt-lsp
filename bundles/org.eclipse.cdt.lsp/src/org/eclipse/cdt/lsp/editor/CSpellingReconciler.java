f1cb05c3e7a6dd975ac05344a2dfcbf79e0e53ab
Gesa Hentschke <gesa.hentschke@bachmann.info>
2024-02-09 17:52:13 +0100

    [222] add spell checking support
    - use Reconciler extension to get the ISourceViewer needed to
    instantiate SpellingReconcileStrategy.
    
    fixes #222

---
+++ bundles/org.eclipse.cdt.lsp/src/org/eclipse/cdt/lsp/editor/CSpellingReconciler.java
@@ -0,0 +1,26 @@
+package org.eclipse.cdt.lsp.editor;
+
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.reconciler.IReconcilingStrategy;
+import org.eclipse.jface.text.reconciler.Reconciler;
+import org.eclipse.jface.text.source.ISourceViewer;
+
+public class CSpellingReconciler extends Reconciler {
+	private CSpellingReconcileStrategy strategy;
+
+	@Override
+	public void install(ITextViewer textViewer) {
+		super.install(textViewer);
+		if (textViewer instanceof ISourceViewer sourceViewer) {
+			strategy = new CSpellingReconcileStrategy(sourceViewer);
+			this.setReconcilingStrategy(strategy, IDocument.DEFAULT_CONTENT_TYPE);
+		}
+	}
+
+	@Override
+	public IReconcilingStrategy getReconcilingStrategy(String contentType) {
+		return strategy;
+	}
+
+}
