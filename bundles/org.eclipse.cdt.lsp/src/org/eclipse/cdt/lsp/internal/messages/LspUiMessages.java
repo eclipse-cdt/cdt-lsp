fb51ffbd2d408b8309fd18800750dfa694dbcbf2
Gesa Hentschke <gesa.hentschke@bachmann.info>
2024-02-16 23:48:15 +0100

    [222] add spell checking support
    - add link to spelling preferences page in Editor (LSP) page.
    
    fixes #222

--- bundles/org.eclipse.cdt.lsp/src/org/eclipse/cdt/lsp/internal/messages/LspUiMessages.java
+++ bundles/org.eclipse.cdt.lsp/src/org/eclipse/cdt/lsp/internal/messages/LspUiMessages.java
@@ -24,6 +24,10 @@ public class LspUiMessages extends NLS {
 	}
 
 	public static String NavigatorView_ErrorOnLoad;
+
+	public static String LspEditorConfigurationPage_spelling_link;
+	public static String LspEditorConfigurationPage_spelling_link_tooltip;
+
 	public static String LspEditorConfigurationPage_enable_project_specific;
 	public static String LspEditorConfigurationPage_configure_ws_specific;
 	public static String LspEditorConfigurationPage_preferLspEditor;
