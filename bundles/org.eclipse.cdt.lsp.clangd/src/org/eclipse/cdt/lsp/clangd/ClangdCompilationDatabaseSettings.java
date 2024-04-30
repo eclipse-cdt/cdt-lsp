d4293de605b710f230eff380e9a720661904716e
Gesa Hentschke <gesa.hentschke@bachmann.info>
2024-04-30 14:18:47 +0200

    Add license header

--- bundles/org.eclipse.cdt.lsp.clangd/src/org/eclipse/cdt/lsp/clangd/ClangdCompilationDatabaseSettings.java
+++ bundles/org.eclipse.cdt.lsp.clangd/src/org/eclipse/cdt/lsp/clangd/ClangdCompilationDatabaseSettings.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   See git history
+ *******************************************************************************/
+
 package org.eclipse.cdt.lsp.clangd;
 
 import org.eclipse.core.resources.IProject;
