d985f975d5f2c6b111a2ff75d022f6bd5063c95d
Gesa HENTSCHKE <Gesa.HENTSCHKE@bachmann.info>
2024-05-13 10:19:56 +0200

    [#314] fix broken API
    MyClangdOptionsDefaults has to implement ClangdOptionsDefaults instead
    of extending the now non-API class BuiltinClangdOptionsDefaults
    
    fixes #314

--- examples/org.eclipse.cdt.lsp.examples.preferences/src/org/eclipse/cdt/lsp/examples/preferences/MyClangdOptionsDefaults.java
+++ examples/org.eclipse.cdt.lsp.examples.preferences/src/org/eclipse/cdt/lsp/examples/preferences/MyClangdOptionsDefaults.java
@@ -14,16 +14,51 @@ package org.eclipse.cdt.lsp.examples.preferences;
 
 import java.util.List;
 
-import org.eclipse.cdt.lsp.clangd.BuiltinClangdOptionsDefaults;
 import org.eclipse.cdt.lsp.clangd.ClangdOptionsDefaults;
 import org.osgi.service.component.annotations.Component;
 
 @Component(service = ClangdOptionsDefaults.class, property = { "service.ranking:Integer=100" })
-public class MyClangdOptionsDefaults extends BuiltinClangdOptionsDefaults {
+public class MyClangdOptionsDefaults implements ClangdOptionsDefaults {
 
 	@Override
 	public List<String> additionalOptions() {
 		return List.of("--header-insertion=never", "--default-config");
+	}
+
+	@Override
+	public String clangdPath() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean useTidy() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean useBackgroundIndex() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String completionStyle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean prettyPrint() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String queryDriver() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
